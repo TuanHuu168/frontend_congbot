5050a999b90debd652a1b06246309d5251be9f2f
TuanHuu168 <tuanhuu.thhn@gmail.com>
2025-06-23 15:24:51 +0700

    add docker file

--- vite.config.js
+++ vite.config.js
@@ -7,17 +7,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser'
-  }
 });
